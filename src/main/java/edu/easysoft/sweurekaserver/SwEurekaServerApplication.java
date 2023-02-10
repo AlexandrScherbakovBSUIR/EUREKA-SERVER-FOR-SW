5ab94e3ef563ed5ea5cf8c14f81f7be54b9c0ebe
unknown <alexandrscherbakov231194@gmail.com>
2023-02-10 16:44:07 +0200

    initial commit

---
+++ src/main/java/edu/easysoft/sweurekaserver/SwEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package edu.easysoft.sweurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SwEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwEurekaServerApplication.class, args);
+	}
+
+}
